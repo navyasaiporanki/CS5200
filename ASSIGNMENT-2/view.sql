91e6f3787980c7d97cc89b8d7ce8391b106931b2
navyasaiporanki <55162684+navyasaiporanki@users.noreply.github.com>
2019-10-09 17:34:17 -0400

    Update view.sql
    changed the view file

--- ASSIGNMENT-2/view.sql
+++ ASSIGNMENT-2/view.sql
@@ -1,11 +1,10 @@
-    create view developer_roles_and_privileges as 
-    select   first_name, last_name, username, email, website.name,website.visits, website.updated,
+    select  first_name, last_name, username, email, website.name,website.visits, website.updated,
   wr.role, wp.priviledge, page.title, page.visits as page_visits, page.updated as page_updated , pr.role as page_role,
   pp.priviledge as page_priviledge from
    person p join developer d on p.id= d.id 
-   join website_role wr on (p.id = wr.developer_id)
-   join website  on (website.id = wr.website_id)
-    join website_priviledge wp on (wp.developer_id = wr.developer_id and wp.website_id = wr.website_id)
+   join website_priviledge wp on (wp.developer_id = d.id)
+   join website_role wr on (wp.developer_id = wr.developer_id and wp.website_id = wr.website_id)
+  join website  on (website.id = wp.website_id)
    left join page on (page.id = website.id)
    left join page_role pr on (pr.developer_id  = p.id and pr.page_id = page.id)
     left join page_priviledge pp on (pp.page_id = pr.page_id and pr.developer_id = pp.developer_id)
